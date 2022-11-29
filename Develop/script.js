14f9908934deace6b99c690e0898aee7fd0d5363
Mathew Sida <sida.mathew@gmail.com>
2022-11-28 21:20:11 -0800

    working I think lol

--- Develop/script.js
+++ Develop/script.js
@@ -5,9 +5,10 @@ var lower;
 var uppercasecheck;
 var numcheck;
 var confirmSpecial;
-var userChoices;
+var input;
 var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 var special = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var blankUpper = [];
 
@@ -52,111 +53,112 @@ function generatePassword() {
   
   else 
   { 
-    lower = confirm("Will this contain lower case letters?");
-    console.log("Lower case " + Lower);
-    uppercasecheck = confirm("Will this contain upper case letters?");
+    lower = confirm("Will your password contain lower case letters?");
+    console.log("Lower case " + lowerCase);
+    uppercasecheck = confirm("Will your password contain upper case letters?");
     console.log("Upper case " + uppercasecheck);
-    numcheck = confirm("Will this contain numbers?");
+    numcheck = confirm("Will your password contain numbers?");
     console.log("Number " + numcheck);
-    confirmSpecial = confirm("Will this contain special characters?");
+    confirmSpecial = confirm("Will your password contain special characters?");
     console.log("Special Character " + confirmSpecial);
 
   };
 
   if (!lower && !uppercasecheck && !numcheck && !confirmSpecial) 
   {
-    userChoices = alert("You must choose a criteria");
+    input = alert("You must choose a criteria");
 
 
 
   } else if (lower && uppercasecheck && numcheck && confirmSpecial) {
-    userChoices = lowerCase.concat(upperCase, numbers, special);
-    console.log(userChoices);
+    input = lowerCase.concat(upperCase, numbers, special);
+    console.log(input);
   }
 
 
 
   else if (lower && uppercasecheck && numcheck) {
-    userChoices = lowerCase.concat(upperCase, numbers);
-    console.log(userChoices);
+    input = lowerCase.concat(upperCase, numbers);
+    console.log(input);
   }
 
 
   else if (lower && uppercasecheck && confirmSpecial) {
-    userChoices = lowerCase.concat(upperCase, special);
-    console.log(userChoices);
+    input = lowerCase.concat(upperCase, special);
+    console.log(input);
   }
 
 
   else if (lower && numcheck && confirmSpecial) {
-    userChoices = lowerCase.concat(numbers, special);
-    console.log(userChoices);
+    input = lowerCase.concat(numbers, special);
+    console.log(input);
 
 
   }
   else if (uppercasecheck && numcheck && confirmSpecial) {
-    userChoices = upperCase.concat(numbers, special);
-    console.log(userChoices);
+    input = upperCase.concat(numbers, special);
+    console.log(input);
   }
 
 
 
   else if (lower && uppercasecheck) {
-    userChoices = lowerCase.concat(upperCase);
-    console.log(userChoices);
+    input = lowerCase.concat(upperCase);
+    console.log(input);
   }
 
 
-  else if (lower && numcheck) {
-    userChoices = lowerCase.concat(numbers);
-    console.log(userChoices);
+  else if (lower && numcheck) 
+  {
+    input = lowerCase.concat(numbers);
+    console.log(input);
   }
 
 
   else if (lower && confirmSpecial) {
-    userChoices = lowerCase.concat(special);
-    console.log(userChoices);
+    input = lowerCase.concat(special);
+    console.log(input);
   }
 
 
   else if (uppercasecheck && numcheck) {
-    userChoices = upperCase.concat(numbers);
-    console.log(userChoices);
+    input = upperCase.concat(numbers);
+    console.log(input);
   }
 
 
   else if (uppercasecheck && confirmSpecial) {
-    userChoices = upperCase.concat(special);
-    console.log(userChoices);
+    input = upperCase.concat(special);
+    console.log(input);
   }
 
 
   else if (numcheck && confirmSpecial) {
-    userChoices = numbers.concat(special);
-    console.log(userChoices);
+    input = numbers.concat(special);
+    console.log(input);
   }
 
   else if (lower) {
-    userChoices = lowerCase;
-    console.log(userChoices);
+    input = lowerCase;
+    console.log(input);
   }
 
 
   else if (uppercasecheck) {
-    userChoices = blankUpper.concat(upperCase);
-    console.log(userChoices);
+    input = blankUpper.concat(upperCase);
+    console.log(input);
   }
 
   
   else if (numcheck) {
-    userChoices = numbers;
-    console.log(userChoices);
+    input = numbers;
+    console.log(input);
   }
 
 
   else if (confirmSpecial) {
-    userChoices = special;
-    console.log(userChoices);
+    input = special;
+    console.log(input);
   };
 
 
@@ -164,7 +166,7 @@ function generatePassword() {
   
 
   for (var i = 0; i < passwordLength; i++) {
-    var allChoices = userChoices[Math.floor(Math.random() * userChoices.length)];
+    var allChoices = input[Math.floor(Math.random() * input.length)];
     passwordBlank.push(allChoices);
     console.log(allChoices);
   }
